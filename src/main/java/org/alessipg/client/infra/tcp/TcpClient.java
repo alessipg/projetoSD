8fb20542ec5e08912f2691b510955665a5a87d0a
Gabriel Alessi <gabriel.alessip@gmail.com>
2025-10-15 20:15:11 -0300

    fix(encoder): fixed UTF-8 encoder

--- src/main/java/org/alessipg/client/infra/tcp/TcpClient.java
+++ src/main/java/org/alessipg/client/infra/tcp/TcpClient.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.charset.Charset;
 
 
 public class TcpClient {
@@ -28,9 +29,11 @@ public class TcpClient {
     }
 
     public void setupStreams() throws IOException {
-        out = new PrintWriter(socket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    }
+        this.in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream(), "UTF-8"));
+        this.out = new PrintWriter(socket.getOutputStream(), true, Charset.forName("UTF-8"));
+
+        }
 
     public void disconnect() throws IOException {
         if (socket != null && !socket.isClosed()) {
